5e16c8cede34b072be545b3d85e18ee6f2878b1f
Raymond Pendergraph <ray.pendergraph@gmx.com>
2023-04-23 20:35:35 -0400

    Fixed setup bug

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -23,7 +23,7 @@ function setup {
    source "${ZI_DIR}/bin/zi.zsh"
 
    zi wait lucid for \
-     atinit"ZI_DIR[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+     atinit"ZI[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
         z-shell/F-Sy-H \
      blockf \
         zsh-users/zsh-completions \
