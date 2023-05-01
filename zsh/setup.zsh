ca472754e8777bc00a6309839c8ca46f976801da
Raymond Pendergraph <raypendergraph@gmail.com>
2023-05-01 13:48:41 -0400

    reformat

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -14,10 +14,11 @@ function setup {
    if [ ! -f "${ZI_DIR}/bin/zi.zsh" ]; then
       print -P "%F{33}▓▒░ %F{160}Installing (%F{33}z-shell/zi%F{160})…%f"
       command mkdir -p "${ZI_DIR}" && command chmod go-rwX "${ZI_DIR}"
-      command git clone -q --depth=1 --branch "main" https://github.com/z-shell/zi "${ZI_DIR}/bin" && \
-         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-         print -P "%F{160}▓▒░ The clone has failed.%f%b"
-               source "${ZI_DIR}/bin/zi.zsh"
+      command git clone -q --depth=1 --branch "main" https://github.com/z-shell/zi "${ZI_DIR}/bin" \
+      && print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" \
+      || print -P "%F{160}▓▒░ The clone has failed.%f%b"
+
+      source "${ZI_DIR}/bin/zi.zsh"
 
       zi pack for system-completions
       zi pack for ls_colors
